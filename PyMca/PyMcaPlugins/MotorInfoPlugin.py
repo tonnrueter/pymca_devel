74c1d5d4c4dbf708137b5becece2d9a5fe2cf4ab
Tonn Ruter <truter@esrf.eu>
2013-09-09 11:58:49 +0200

    First Version of MotorInfoPlugin

---
+++ PyMca/PyMcaPlugins/MotorInfoPlugin.py
@@ -0,0 +1,105 @@
+import numpy
+
+try:
+    from PyMca import Plugin1DBase
+except ImportError:
+    from . import Plugin1DBase
+
+try:
+    from PyMca import MotorInfoWindow
+except ImportError:
+    print("MotorInfoPlugin importing from somewhere else")
+    import MotorInfoWindow
+    
+DEBUG = 1
+
+class MotorInfo(Plugin1DBase.Plugin1DBase):
+    def __init__(self,  plotWindow,  **kw):
+        Plugin1DBase.Plugin1DBase.__init__(self,  plotWindow,  **kw)
+        self.methodDict = {}
+        text = 'Show values of different motors.'
+        function = self.showMotorInfo
+        icon = None
+        info = text
+        self.methodDict["Show Motor Info"] =[function, info, icon]
+        self.widget = None
+    
+    def getMethods(self, plottype=None):
+        names = list(self.methodDict.keys())
+        names.sort()
+        return names
+
+    def getMethodToolTip(self, name):
+        return self.methodDict[name][1]
+
+    def getMethodPixmap(self, name):
+        return self.methodDict[name][2]
+
+    def applyMethod(self, name):
+        self.methodDict[name][0]()
+        return
+
+    def showMotorInfo(self):
+        curves = self.getAllCurves()
+        nCurves = len(curves)
+        if DEBUG:
+            print ("Received %d curve(s).." % nCurves)
+        if nCurves:
+            self.legendsList = [leg for (xvals, yvals,  leg,  info) in curves] 
+            infoList = [info for (xvals, yvals,  leg,  info) in curves] 
+            self.motorValuesList = self._convertInfoDictionary( infoList )
+        else:
+            raise ValueError("No Curve(s) present.")
+        if self.widget is None:
+            self._createWidget()
+#        ret = self.widget.exec_()
+        ret = self.widget.show()
+
+    def _convertInfoDictionary(self,  infosList):
+        ret = []
+        for info in infosList :
+            motorNames = info.get('MotorNames',  None)
+            if motorNames is not None:
+                if type(motorNames) == str:
+                    namesList = motorNames.split()
+                elif type(motorNames) == list:
+                    namesList = motorNames
+                else:
+                    namesList = []
+            else:
+                namesList = []
+            motorValues = info.get('MotorValues',  None)
+            if motorNames is not None:
+                if type(motorValues) == str:
+                    valuesList = motorValues.split()
+                elif type(motorValues) == list:
+                    valuesList = motorValues
+                else:
+                    valuesList = []
+            else:
+                valuesList = []
+            if len(namesList) == len(valuesList):
+                ret.append( dict( zip( namesList,  valuesList ) ) )
+            else:
+                print("Number of motors and values does not match!")
+        return ret
+    
+    def _createWidget(self):
+        parent = None
+        self.widget = MotorInfoWindow.MotorInfoWidget(parent,  self.legendsList,  self.motorValuesList)
+
+MENU_TEXT = "Motor Info"
+def getPlugin1DInstance(plotWindow,  **kw):
+    print("Creating MotorInfo..")
+    ob = MotorInfo(plotWindow)
+    return ob
+
+if __name__ == "__main__":
+    # Basic test setup
+    from PyMca import Plot1D
+    x = numpy.arange(100.)
+    y = x * x
+    plot = Plot1D.Plot1D()
+    plot.addCurve(x, y, "Curve1", {'MotorNames': "foo bar",  'MotorValues': "3.14 2.97"})
+    plot.addCurve(x+100, -x*x, "Curve2", {'MotorNames': "baz",  'MotorValues': "6.28"})
+    plugin = getPlugin1DInstance(plot)
